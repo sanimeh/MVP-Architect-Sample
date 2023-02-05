178353d4fffc006ae9b7e9f898ddc24e9fd8a18a
Husam <husam.abusanimeh@gmail.com>
2023-02-05 22:23:30 +0100

    SDK Done

--- sdk/src/main/java/com/laces/app/sdk/Sdk.kt
+++ sdk/src/main/java/com/laces/app/sdk/Sdk.kt
@@ -1,20 +1,25 @@
 package com.laces.app.sdk
 
+import com.laces.app.sdk.api.ProductsApi
+import com.laces.app.sdk.model.ProductModel
+import com.laces.app.sdk.network.RetrofitBuilder
 import javax.inject.Singleton
 
 interface Sdk {
-    fun getProducts()
-    fun getDetails(id: String)
+    suspend fun getProducts(): List<ProductModel>
+    suspend fun getDetails(id: Int): ProductModel
 }
 
 @Singleton
-internal class SdkImpl : Sdk {
+class SdkImpl : Sdk {
 
-    override fun getProducts() {
-
+    override suspend fun getProducts(): List<ProductModel> {
+        val quotesApi = RetrofitBuilder.getInstance().create(ProductsApi::class.java)
+        return quotesApi.getWrapperProduct().body()?.products!!
     }
 
-    override fun getDetails(id: String) {
-
+    override suspend fun getDetails(id: Int): ProductModel {
+        val quotesApi = RetrofitBuilder.getInstance().create(ProductsApi::class.java)
+        return quotesApi.getDetails(id)
     }
 }
